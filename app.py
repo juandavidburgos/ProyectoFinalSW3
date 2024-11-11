1f9d29f1c24dff3ed0752049f6d3bcba82f2fcac
LauraSofi05 <laubotina@unicauca.edu.co>
2024-11-10 22:39:27 -0500

    app.py modificado

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 #Aplicacion principal
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+app.secret_key = 'flask'
+
+@app.route('/')
+def index():
+    return print("Hola Bellas")
+if __name__ == '__main__':
+    app.run(debug=True)
